aa80861708cfc706c2885217c63609e35299f31e
Dan Massarano <danielmassarano@gmail.com>
2018-09-17 18:14:46 +0100

    apparently this is the way to do it

--- getruby.cmd
+++ getruby.cmd
@@ -59,7 +59,7 @@ cd
 curl -L -o update.gem https://rubygems.org/downloads/rubygems-update-2.6.7.gem
 call gem install --local update.gem
 call update_rubygems --no-ri --no-rdoc > updaterubygemsout
-call gem uninstall rubygems-update -x 3
+call 3 | gem uninstall rubygems-update -x
 ECHO What's our new Rubygems version?
 call gem --version
 REM ECHO Updating Rubygems...
